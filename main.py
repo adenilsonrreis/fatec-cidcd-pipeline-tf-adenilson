fb3295d6b2337fb74d75cc06a5ce83470f6de7f6
adenilsonrreis <reisadenilson300@gmail.com>
2026-05-26 23:41:49 -0300

    Add calcular_media function and update main execution

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def saudacao(nome: str) -> str:
         raise TypeError("Nome deve ser uma string")
     return f"Olá, {nome}! Bem-vindo ao sistema."
 
+
 def calcular_media(notas: list) -> float:
     """Calcula a média de uma lista de notas."""
     if not notas:
@@ -11,5 +12,6 @@ def calcular_media(notas: list) -> float:
     return sum(notas) / len(notas)
 
 if __name__ == "__main__":
+    
     print(saudacao("Aluno FATEC"))
     print(f'Média: {calcular_media([8.5, 9.0, 7.5])}')
