2e8b6cb29e05f4c9ffc78ecaab083a17e43e20fe
adenilsonrreis <reisadenilson300@gmail.com>
2026-05-27 00:49:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
 
 def buscar_usuario_vulneravel(user_id):
-    conn = sqlite3.connect("banco.db")
+    conn = sqlite3.connect('banco.db')
     cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM users WHERE id={user_id}")
     return cursor.fetchone()
